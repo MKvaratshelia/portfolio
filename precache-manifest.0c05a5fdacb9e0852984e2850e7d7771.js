0a13f84c44615ee563c5d894227e730bd5f36c2f
Kvaratshelia Mihail <kvaratsheliamihail@yandex.ru>
2020-06-06 18:50:48 +0300

    Updates

---
+++ precache-manifest.0c05a5fdacb9e0852984e2850e7d7771.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31bd7213adc262cd869d5d381ff6759",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7ef4a40b3324ec66f48e",
+    "url": "/portfolio/static/css/main.612524d9.chunk.css"
+  },
+  {
+    "revision": "d22614215346f6fe65e0",
+    "url": "/portfolio/static/js/2.3baec81c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.3baec81c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef4a40b3324ec66f48e",
+    "url": "/portfolio/static/js/main.140359c9.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "ab2e20344fd28bddaed45491f084465e",
+    "url": "/portfolio/static/media/IMG_avatar.ab2e2034.jpg"
+  },
+  {
+    "revision": "20119b8063c15423b7fe6a85bf534118",
+    "url": "/portfolio/static/media/diplom.20119b80.jpg"
+  },
+  {
+    "revision": "aaaf531a1b6d28ab6e420dc43da0f5eb",
+    "url": "/portfolio/static/media/github-logo.aaaf531a.svg"
+  },
+  {
+    "revision": "ecb9d5f02d3650c9ab6e86f22c175a45",
+    "url": "/portfolio/static/media/learn.ecb9d5f0.jpg"
+  },
+  {
+    "revision": "e3c80e2b2449d0162c762a2b5961b380",
+    "url": "/portfolio/static/media/mail.e3c80e2b.svg"
+  },
+  {
+    "revision": "2dd5af863664d8d8e009f8b88c333980",
+    "url": "/portfolio/static/media/mesto.2dd5af86.jpg"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "/portfolio/static/media/roboto-medium.6422eac7.woff"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/portfolio/static/media/roboto-regular.58bf8e0b.woff"
+  },
+  {
+    "revision": "0ad272c4fea700685e7146c11accd8f3",
+    "url": "/portfolio/static/media/russia.0ad272c4.jpg"
+  },
+  {
+    "revision": "578398cee8105e54986283266ceee98a",
+    "url": "/portfolio/static/media/tel.578398ce.svg"
+  }
+]);
